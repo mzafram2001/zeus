62082b1652d843dc19f60c7e980654a9d081af03
Miguel Zafra <mzafram2001@gmail.com>
2024-02-14 19:52:00 +0100

    UPDATE sidebar components with mor bootstrap modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersComponent } from './users/users.component';
+import { ChangelogComponent } from './changelog/changelog.component';
 
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'changelog', component: ChangelogComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
