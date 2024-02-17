762a45e0fd076b5dd96f6dbe9a1c607f06060c38
Miguel Zafra <mzafram2001@gmail.com>
2024-02-17 11:40:56 +0100

    UPDATE dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import Chart from 'chart.js/auto';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public chartSales: any;
   public chartStats: any;
 
   constructor(private router: Router) { }
@@ -20,14 +21,26 @@ export class DashboardComponent implements OnInit {
     this.router.navigate(['/changelog']);
   }
 
-  public createChart() {
-    const DATA_COUNT = 7;
-    const NUMBER_CFG = { count: DATA_COUNT, min: 10, max: 50 };
+  onStatsButtonClick() {
+    this.router.navigate(['/stats']);
+  }
 
-    this.chartStats = new Chart("stats", {
+  public createChart() {
+    this.chartSales = new Chart("sales", {
       type: 'line',
       data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        labels: ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'],
         datasets: [
           {
             label: 'Total sales',
@@ -45,12 +58,64 @@ export class DashboardComponent implements OnInit {
       },
       options: {
         responsive: true,
+        devicePixelRatio: 2,
         aspectRatio: 2.5,
         scales: {
           y: {
             suggestedMin: 10,
             suggestedMax: 50
           }
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+
+    this.chartStats = new Chart("stats", {
+      type: 'radar',
+      data: {
+        labels: [
+          '0-5 years',
+          '6-11 years',
+          '12-18 years',
+          '14-26 years',
+          '27-59 years',
+          '+60 years',
+        ],
+        datasets: [{
+          label: 'Year 2023',
+          data: [25, 21, 26, 34, 25, 19],
+          fill: true,
+          backgroundColor: 'rgba(244, 185, 66, 0.2)',
+          borderColor: '#F4B942',
+          pointBackgroundColor: '#F4B942',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: '#F4B942'
+        }, {
+          label: 'Year 2024',
+          data: [16, 20, 30, 32, 19, 27],
+          fill: true,
+          backgroundColor: 'rgba(30, 30, 30, 0.2)',
+          borderColor: '#1E1E1E',
+          pointBackgroundColor: '#1E1E1E',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: '#1E1E1E'
+        }]
+      },
+      options: {
+        responsive: true,
+        devicePixelRatio: 2,
+        aspectRatio: 1,
+        scales: {
+          r: {
+            min: 10,
+            max: 35,
+          },
         },
         plugins: {
           legend: {
