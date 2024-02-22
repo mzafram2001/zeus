d4299333f950337dd64d832032a3de949092a645
Miguel Zafra <mzafram2001@gmail.com>
2024-02-22 20:26:31 +0100

    UPDATE dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -63,7 +63,7 @@ export class DashboardComponent implements OnInit {
         scales: {
           y: {
             suggestedMin: 10,
-            suggestedMax: 50
+            suggestedMax: 5
           }
         },
         plugins: {
