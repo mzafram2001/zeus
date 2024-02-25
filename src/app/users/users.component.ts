f6d3555f9ae541e01464c72ad50d2ed3146460c7
Miguel Zafra <mzafram2001@gmail.com>
2024-02-25 11:12:36 +0100

    UPDATE users component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  public chartReputation: any;
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  public createChart() {
+    this.chartReputation = new Chart("reputation", {
+      type: 'doughnut',
+      data: {
+        labels: ['Completed % to rank up', 'Remaining % to rank up'],
+        datasets: [
+          {
+            data: [72, 28],
+            borderColor: ['#F4B942', '#1E1E1E'],
+            backgroundColor: ['rgba(244, 185, 66, 0.5)', 'rgba(30, 30, 30, 0.5)'],
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        devicePixelRatio: 2,
+        aspectRatio: 1.5,
+        layout: {
+          padding: 20
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+  }
 }
