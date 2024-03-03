c9fe67ea271319ef8227b4dc0aa8790ef5366c65
Miguel Zafra <mzafram2001@gmail.com>
2024-03-03 11:05:36 +0100

    UPDATE to version 0.0.21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersComponent } from './users/users.component';
 import { ChangelogComponent } from './changelog/changelog.component';
+import { PaletteComponent } from './palette/palette.component';
 
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UsersComponent },
   { path: 'changelog', component: ChangelogComponent },
+  { path: 'palette', component: PaletteComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
