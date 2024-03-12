f800bdfed9d2f34e92c309a71834cc2791afdadc
Miguel Zafra <mzafram2001@gmail.com>
2024-03-12 19:47:32 +0100

    ADD tooltip support on desktop, UPDATE header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl, {
+        'customClass': 'tooltip-zeus'
+      });
+    })
+  }
 }
